d0c53e7381e9f86e4c1284d53355450cdb0c99f6
Vihaan Jagiasi <88507509+VJagiasi@users.noreply.github.com>
2023-08-02 15:33:39 +0530

    made navbar sticky

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-around bg-background h-28 p-10" >
+    <div className="flex justify-around bg-background h-28 p-10 sticky top-0 shadow  " >
         <h2 className="text-2xl text-white md:text-2xl font-rubik font-bold">Home</h2>
         <ul className="flex justify-center text-2xl text-white font-bold font-rubik gap-4">
             <li>About</li>
