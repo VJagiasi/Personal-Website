036269c6d7280f53d90f349a42957b54d13b0a92
Vihaan Jagiasi <88507509+VJagiasi@users.noreply.github.com>
2023-08-05 23:46:59 +0530

    added links and hovering effects

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-around bg-background h-28 p-10 sticky top-0 shadow  " >
-        <h2 className="text-2xl text-white md:text-2xl font-rubik font-bold">Home</h2>
-        <ul className="flex justify-center text-2xl text-white font-bold font-rubik gap-4">
-            <li>About</li>
-            <li>Experience</li>
-            <li>Projects</li>
-            <li>Resume</li>
-        </ul>
+    <div className="flex justify-around items-center bg-background h-25 p-7 sticky top-0 shadow" >
+          <h2 className="text-xl text-white md:text-xl font-rubik font-bold">Home</h2>
+          <ul className="flex justify-center text-xl text-white font-bold font-rubik gap-4">
+              <li><Link to="about" spy={true} smooth={true} className="nav-link">About</Link></li>
+              <li><Link to="experience" spy={true} smooth={true} className="nav-link">Experience</Link></li>
+              {/* <li>Projects</li> */}
+              <li><Link to="contact" spy={true} smooth={true} className="nav-link">Contact</Link></li>
+              <a href="https://drive.google.com/file/d/1gh3ddSXLany8HAJIQC4aOxZF5R0tyUC5/view?usp=sharing" target="_blank" className="nav-link"><li>Resume</li></a>
+          </ul>
     </div>
   )
 }
