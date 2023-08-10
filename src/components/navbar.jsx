fc344af86e4c628c2615ffad07144e658a2a7462
Vihaan Jagiasi <88507509+VJagiasi@users.noreply.github.com>
2023-08-11 00:34:55 +0530

    removed underline hover effect in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-scroll';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
-
-
 const Navbar = () => {
   const [theme, setTheme] = useState("light");
   const changetheme = () => {
@@ -30,15 +28,17 @@ const Navbar = () => {
           <h2 className="text-3xl text-white dark:text-black font-rubik font-bold">Home</h2>
           <ul className="flex justify-center text-xl text-white dark:text-black font-bold font-rubik gap-4">
               <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none">
-                <li><Link to="about" spy={true} smooth={true} className="nav-link text-2xl">About</Link></li>
+                <li><Link to="about" spy={true} smooth={true} className="text-2xl">About</Link></li>
               </div>
               <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none">
-                <li><Link to="experience" spy={true} smooth={true} className="nav-link text-2xl">Experience</Link></li>
+                <li><Link to="experience" spy={true} smooth={true} className="
+                text-2xl">Experience</Link></li>
               </div>
               <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none">
-                <li><Link to="contact" spy={true} smooth={true} className="nav-link text-2xl">Contact</Link></li>
+                <li><Link to="contact" spy={true} smooth={true} className="
+                text-2xl">Contact</Link></li>
               </div>
-              <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none"><a href="https://drive.google.com/file/d/1gh3ddSXLany8HAJIQC4aOxZF5R0tyUC5/view?usp=sharing" target="_blank" rel="noreferrer" className="nav-link text-2xl"><li>Resume</li></a></div>
+              <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none"><a href="https://drive.google.com/file/d/1gh3ddSXLany8HAJIQC4aOxZF5R0tyUC5/view?usp=sharing" target="_blank" rel="noreferrer" className="text-2xl"><li>Resume</li></a></div>
               <div className="transition hover:-translate-y-1 hover: motion-reduce:transition-none motion-reduce:hover:transform-none">
                 <button type="button" onClick={changetheme} className="ml-4">
                   <FontAwesomeIcon icon={theme === "dark"? faMoon:faSun} size="xl"></FontAwesomeIcon>
